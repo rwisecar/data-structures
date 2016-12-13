9ce813cf4e766ccb3312a41ca904b2423a2259fc
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-12-13 05:01:08 -0800

    refactored Node and LinkedList class inits to allow for input of full lists of data

--- src/linked_list.py
+++ src/linked_list.py
@@ -3,36 +3,57 @@
 
 class Node(object):
     """Create node instances that can populate the linked list."""
-    def __init__(self, val=None, next=None):
-        self.val = val
-        self.next = next
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList(object):
 
-    def __init__(self, head=None, length=0):
+    def __init__(self, head=None, data=None, length=0):
         """Create a linked list based on input nodes."""
-        self.head = head
         self.length = length
+        # self.head = None
+        if data:
+            try:
+                for item in data:
+                    if item is data[0]:
+                        self.head = Node(item)
+                    else:
+                        self.head = Node(item, self.head)
+            except TypeError:
+                self.head = Node(data)
+        else:
+            self.head = head
+        # if data:
+        #     try:
+        #         for d in data:
+        #             if d is data[0]:
+        #                 self.head = Node(d)
+        #             else:
+        #                 self.head = Node(d, self.head)
+        #     except TypeError:
+        #         self.head = Node(data)
+        # self.head = head
 
-    def push(self, val):
-        """Add a node as the new head of a linked list."""
-        node = Node(val)
-        node.next = self.head
-        self.head = node
-        self.length = self.length + 1
+#     def push(self, val):
+#         """Add a node as the new head of a linked list."""
+#         node = Node(val)
+#         node.next = self.head
+#         self.head = node
+#         self.length = self.length + 1
 
-    def pop(self):
-        """Remove and return head of a linked list."""
-        next_node = self.head.next
-        old_head = self.head
-        self.head = next_node
-        self.length = self.length - 1
-        return old_head.val
+#     def pop(self):
+#         """Remove and return head of a linked list."""
+#         next_node = self.head.next
+#         old_head = self.head
+#         self.head = next_node
+#         self.length = self.length - 1
+#         return old_head.val
 
-    def size(self):
-        """Return the length of the linked list."""
-        return self.length
+#     def size(self):
+#         """Return the length of the linked list."""
+#         return self.length
 
     # def remove(self, node):
     #     """Input a node and remove that node from the list."""
@@ -52,6 +73,24 @@ class LinkedList(object):
     #     else:
     #         raise ValueError
 
+"""Remove function found at codefellows.org"""
+    # def delete(self, data):
+    # current = self.head
+    # previous = None
+    # found = False
+    # while current and found is False:
+    #     if current.get_data() == data:
+    #         found = True
+    #     else:
+    #         previous = current
+    #         current = current.get_next()
+    # if current is None:
+    #     raise ValueError("Data not in list")
+    # if previous is None:
+    #     self.head = current.get_next()
+    # else:
+    #     previous.set_next(current.get_next())
+
     # def search(self, value):
     #     """Search through list for a node and return that node."""
     #     current = self.head
@@ -64,5 +103,18 @@ class LinkedList(object):
     #             current = current.next
     #         return None
     #     return None
+
+"""Search function found at https://www.codefellows.org/blog/implementing-a-singly-linked-list-in-python/"""
+    # def search(self, data):
+    # current = self.head
+    # found = False
+    # while current and found is False:
+    #     if current.get_data() == data:
+    #         found = True
+    #     else:
+    #         current = current.get_next()
+    # if current is None:
+    #     raise ValueError("Data not in list")
+    # return current
 
     # def display()
--- src/test_linked_list.py
+++ src/test_linked_list.py
@@ -7,52 +7,58 @@ from linked_list import Node, LinkedList
 def test_create_node():
     """Test whether we create a node with a value 'val'."""
     test_node = Node(15)
-    assert test_node.val == 15
+    assert test_node.data == 15
 
 
 def test_create_list():
     """Test whether we create a new Linked List instance."""
-    new_node = Node(val=15, next=None)
-    new_list = LinkedList(head=new_node, length=15)
-    assert new_list.head == new_node and new_list.length == 15
+    new_node = Node(data=15, next_node=None)
+    new_list = LinkedList(head=new_node, data=None, length=1)
+    assert new_list.head == new_node and new_list.length == 1
 
 
-def test_create_size():
-    """Test whether length of the link link"""
-    linked_list1 = LinkedList(head=2, length=10)
-    assert linked_list1.size() == linked_list1.length
+def test_create_from_existing_list():
+    """Test whether we can create a linked list from an existing list."""
+    new_list = LinkedList(head=None, data=[1], length=3)
+    new_node = Node(data=1)
+    assert new_list.head.data == new_node.data
+
+# def test_size():
+#     """Test whether length of the link link"""
+#     linked_list1 = LinkedList(head=2, length=10)
+#     assert linked_list1.size() == linked_list1.length
 
 
-def test_create_push():
-    """Test whether the push method adds a node to the end."""
-    node1 = Node(15)
-    node2 = Node(10)
-    new_list = LinkedList(node1, length=1)
-    new_list.push(node2)
-    assert new_list.length == 2
+# def test_push():
+#     """Test whether the push method adds a node to the end."""
+#     node1 = Node(15)
+#     node2 = Node(10)
+#     new_list = LinkedList(node1, length=1)
+#     new_list.push(node2)
+#     assert new_list.length == 2
 
 
-def test_pop():
-    """Test whether the pop method removes and returns the first node."""
-    node1 = Node(15)
-    node2 = Node(10)
-    node3 = Node(5)
-    new_list = LinkedList(node1, length=1)
-    new_list.push(node2)
-    new_list.push(node3)
-    assert new_list.pop().val == node3.val
+# def test_pop():
+#     """Test whether the pop method removes and returns the first node."""
+#     node1 = Node(15)
+#     node2 = Node(10)
+#     node3 = Node(5)
+#     new_list = LinkedList(node1, length=1)
+#     new_list.push(node2)
+#     new_list.push(node3)
+#     assert new_list.pop().val == node3.val
 
 
-def test_pop_length():
-    """Test whether the length of the list changes when a value is popped."""
-    node1 = Node(15)
-    node2 = Node(10)
-    node3 = Node(5)
-    new_list = LinkedList(node1, length=1)
-    new_list.push(node2)
-    new_list.push(node3)
-    new_list.pop()
-    assert new_list.length == 2
+# def test_pop_length():
+#     """Test whether the length of the list changes when a value is popped."""
+#     node1 = Node(15)
+#     node2 = Node(10)
+#     node3 = Node(5)
+#     new_list = LinkedList(node1, length=1)
+#     new_list.push(node2)
+#     new_list.push(node3)
+#     new_list.pop()
+#     assert new_list.length == 2
 
 
 # def test_remove_length():
