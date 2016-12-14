10d909c1f95bf525f0315358f316d6f98e1d41fc
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-12-14 09:43:35 -0800

    Refactoring docstrings in Stack class.

--- src/stack.py
+++ src/stack.py
@@ -1,9 +1,14 @@
 """Create a stack class to inherit from LinkedList class and module."""
-from linked_list import LinkedList, Node
+from linked_list import LinkedList
 
 
 class Stack(object):
-    """Create a stack that inherits select functions from LinkedList"""
+    """Create a stack that inherits select functions from LinkedList
+
+    push(val): adds a new node containing val to the Stack.
+    pop(): removes and returns the top node from the Stack.
+    size(): returns the length of the items in th Stack.
+    """
 
     def __init__(self, head=None, data=None, length=0):
         """Create a new stack based on LinkedList composition."""
@@ -21,4 +26,5 @@ class Stack(object):
             raise IndexError("Cannot pop from an empty stack")
 
     def size(self):
+        """Returns the length of the stack."""
         self._linkedlist.size()
