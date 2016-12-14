d4de97fa77fe2164030e400ef735abe5bf83db94
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-12-14 10:05:55 -0800

    adding fixtures to Stack tests.

--- src/test_stack.py
+++ src/test_stack.py
@@ -3,6 +3,14 @@
 import pytest
 
 
+@pytest.fixture
+def new_stack():
+    """Create a new stack with some data as a fixture."""
+    from stack import Stack
+    this_stack = Stack(data=[1, 2, 3])
+    return this_stack
+
+
 def test_new_stack_is_empty():
     """Check that a new stack is empty."""
     from stack import Stack
@@ -10,18 +18,9 @@ def test_new_stack_is_empty():
     assert this_stack._linkedlist.size() == 0
 
 
-def test_new_list_has_zero_size():
-    """Check that a new list has no size."""
-    from stack import Stack
-    this_stack = Stack()
-    assert this_stack._linkedlist.size() == 0
-
-
-def test_new_stack_with_data_has_data():
+def test_new_stack_with_data_has_data(new_stack):
     """Check new stack with has data."""
-    from stack import Stack
-    this_stack = Stack(data=[1, 2, 3])
-    assert this_stack._linkedlist.head.value == 3
+    assert new_stack._linkedlist.head.value == 3
 
 
 def test_new_stack_with_head_has_head():
@@ -33,50 +32,38 @@ def test_new_stack_with_head_has_head():
     assert this_stack._linkedlist.head.value == 5
 
 
-def test_pushed_value_is_new_head():
+def test_pushed_value_is_new_head(new_stack):
     """When pushing to stack, the pushed value should be the new head."""
-    from stack import Stack
-    this_stack = Stack(data=[1, 2, 3])
-    this_stack._linkedlist.push("new")
-    assert this_stack._linkedlist.head.value == "new"
+    new_stack._linkedlist.push("new")
+    assert new_stack._linkedlist.head.value == "new"
 
 
-def test_length_of_stack():
+def test_length_of_stack(new_stack):
     """Check that size of the stack reflects the number of nodes in the stack."""
-    from stack import Stack
-    this_stack = Stack(data=[1, 2, 3])
-    assert this_stack._linkedlist.length == 3
+    assert new_stack._linkedlist.length == 3
 
 
-def test_length_of_stack_when_pushed():
+def test_length_of_stack_when_pushed(new_stack):
     """Check that size changes to reflect pushed values."""
-    from stack import Stack
-    this_stack = Stack(data=[1, 2, 3])
-    this_stack._linkedlist.push("new")
-    assert this_stack._linkedlist.length == 4
+    new_stack._linkedlist.push("new")
+    assert new_stack._linkedlist.length == 4
 
 
-def test_size_of_stack_with_nodes():
+def test_size_of_stack_with_nodes(new_stack):
     """Check that the size of a nonempty stack is accurate."""
-    from stack import Stack
-    this_stack = Stack(data=[1, 2, 3])
-    assert this_stack._linkedlist.size() == 3
+    assert new_stack._linkedlist.size() == 3
 
 
-def test_size_of_stack_when_pushed():
+def test_size_of_stack_when_pushed(new_stack):
     """Check that size changes to reflect pushed values."""
-    from stack import Stack
-    this_stack = Stack(data=[1, 2, 3])
-    this_stack._linkedlist.push("new")
-    assert this_stack._linkedlist.size() == 4
+    new_stack._linkedlist.push("new")
+    assert new_stack._linkedlist.size() == 4
 
 
-def test_size_of_stack_when_popped():
+def test_size_of_stack_when_popped(new_stack):
     """Check that size decreases when popped."""
-    from stack import Stack
-    this_stack = Stack(data=[1, 2, 3])
-    this_stack._linkedlist.pop()
-    assert this_stack._linkedlist.size() == 2
+    new_stack._linkedlist.pop()
+    assert new_stack._linkedlist.size() == 2
 
 
 def test_pop_with_values():
