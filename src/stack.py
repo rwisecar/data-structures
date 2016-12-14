f7be84633771b039f208afdcea81b3ebca2069ec
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-12-14 09:21:59 -0800

    Refactoring code after code review in class.

--- src/stack.py
+++ src/stack.py
@@ -7,23 +7,18 @@ class Stack(object):
 
     def __init__(self, head=None, data=None, length=0):
         """Create a new stack based on LinkedList composition."""
-        self._linkedlist = LinkedList()
-        if data:
-            try:
-                for item in data:
-                    self._linkedlist.head = Node(item)
-                    self._linkedlist.length += 1
-            except TypeError:
-                self._linkedlist.head = data
-        else:
-            self._linkedlist.head = head
-            self._linkedlist.length = length
+        self._linkedlist = LinkedList(head, data, length)
 
     def push(self, value):
+        """Push new values to head of stack."""
         self._linkedlist.push(value)
 
     def pop(self):
-        self._linkedlist.pop()
+        """Pop first value from top of stack and return."""
+        try:
+            return self._linkedlist.pop()
+        except IndexError:
+            raise IndexError("Cannot pop from an empty stack")
 
     def size(self):
         self._linkedlist.size()
