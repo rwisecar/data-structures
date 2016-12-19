a83320d2757eabfe87a9b2f68dab13df6699bf79
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-12-19 14:15:25 -0800

    adding deque and test modules, with init, size and append fcns.

--- src/test_deque.py
+++ src/test_deque.py
@@ -35,7 +35,7 @@ def test_full_deque_has_length(full_deque):
 
 def test_empty_deque_is_empty(empty_deque):
     """Test that empty deque is empty."""
-    assert empty_deque._deque._length == 0
+    assert empty_deque.size() == 0
 
 
 def test_initializing_deque_with_non_iterable_raises_error():
@@ -43,3 +43,33 @@ def test_initializing_deque_with_non_iterable_raises_error():
     from deque import Deque
     with pytest.raises(TypeError):
         new_deque = Deque(interable=123456)
+
+
+def test_append_to_empty_deque_check_head(empty_deque):
+    empty_deque.append("Denny Way")
+    assert empty_deque._deque.head.value == "Denny Way"
+
+
+def test_append_to_empty_deque_check_tail(empty_deque):
+    empty_deque.append("Denny Way")
+    assert empty_deque._deque.tail.value == "Denny Way"
+
+
+def test_append_to_empty_deque_size(empty_deque):
+    empty_deque.append("Denny Way")
+    assert empty_deque.size() == 1
+
+
+def test_append_to_full_deque_check_head(full_deque):
+    full_deque.append("Denny Way")
+    assert full_deque._deque.head.value == 3
+
+
+def test_append_to_full_deque_check_tail(full_deque):
+    full_deque.append("Denny Way")
+    assert full_deque._deque.tail.value == "Denny Way"
+
+
+def test_append_to_full_deque_size(full_deque):
+    full_deque.append("Denny Way")
+    assert full_deque.size() == 4
