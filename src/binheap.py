27bfbd21be733d7f4d0f1c83541da271d20fdeff
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-12-20 12:59:06 -0800

    adding framework for binheap and test files.

---
+++ src/binheap.py
@@ -0,0 +1,23 @@
+"""Create binary heap in which parent nodes are smaller than their children."""
+
+
+class Binheap(object):
+    """Create a binary heap in which parent node is smallest.
+
+    push() adds a new value into the heap.
+
+    pop() removes the parent value from the top of the heap.
+
+    Both push() and pop() maintain heap property."""
+
+    def __init__(self, iterable=None):
+        """Instantiate a new binary heap, empty or with an iterable."""
+        pass
+
+    def push(self, value):
+        """Adds a value at the end of the heap into the leftmost empty spot."""
+        pass
+
+    def pop(self):
+        """Removes the parent element from the top of the heap and reorders."""
+        pass
