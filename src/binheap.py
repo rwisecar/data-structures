4d1322f39c1e0dcd37c82164f22365092b49ed46
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-12-20 14:29:49 -0800

    trying to figure out how it works

--- src/binheap.py
+++ src/binheap.py
@@ -11,13 +11,28 @@ class Binheap(object):
     Both push() and pop() maintain heap property."""
 
     def __init__(self, iterable=None):
-        """Instantiate a new binary heap, empty or with an iterable."""
-        pass
+        self.heap = []
+        if iterable and hasattr(iterable, "__iter__"):
+            self.heap.extend(iterable)
+        elif iterable:
+            raise TypeError("Can't init with a non iterable.")
 
-    def push(self, value):
-        """Adds a value at the end of the heap into the leftmost empty spot."""
-        pass
+    # def push(self, value):
+    #     """Adds a value at the end of the heap into the leftmost empty spot."""
+    #     if not self.most_min:
+    #         self.most_min = value
+    #     elif self.most_min:
+    #         while value < self[(len(self) - 1 // 2) - 1]:
+    #             new_add = value
+    #             old_parent = self[(len(self) - 1 // 2) - 1]
+    #             new_add, old_parent = old_parent, new_add
+    #     # self.current_size += 1
 
-    def pop(self):
+
+
+    # def pop(self):
         """Removes the parent element from the top of the heap and reorders."""
+        # Find and remove min value
+        # Take most recent node and add to head
+        # Resort
         pass
