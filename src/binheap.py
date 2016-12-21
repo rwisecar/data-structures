e1a58851526a22be96a9a3b7804e4f7001af3570
Amos Boldor <amosboldor@gmail.com>
2016-12-20 16:29:38 -0800

    some work we did

--- src/binheap.py
+++ src/binheap.py
@@ -11,24 +11,29 @@ class Binheap(object):
     Both push() and pop() maintain heap property."""
 
     def __init__(self, iterable=None):
+        """The init function for Binheap."""
         self.heap = []
         if iterable and hasattr(iterable, "__iter__"):
             self.heap.extend(iterable)
         elif iterable:
             raise TypeError("Can't init with a non iterable.")
 
-    # def push(self, value):
-    #     """Adds a value at the end of the heap into the leftmost empty spot."""
-    #     if not self.most_min:
-    #         self.most_min = value
-    #     elif self.most_min:
-    #         while value < self[(len(self) - 1 // 2) - 1]:
-    #             new_add = value
-    #             old_parent = self[(len(self) - 1 // 2) - 1]
-    #             new_add, old_parent = old_parent, new_add
-    #     # self.current_size += 1
+    # def sort_up():
+    #     """Sort up."""
 
-
+    def push(self, value):
+        """Add a value at the end of the heap into the leftmost empty spot."""
+        parent = (len(self.heap) - 1 // 2) - 1
+        if len(self.heap) == 0:
+            self.heap.append(value)
+        else:
+            self.heap.append(value)
+            child = self.heap[-1]
+            while child < self.heap[parent]:
+                self.heap[value]
+                child, parent = parent, child
+                parent // 2
+                
 
     # def pop(self):
         """Removes the parent element from the top of the heap and reorders."""
--- src/test_binheap.py
+++ src/test_binheap.py
@@ -1,1 +1,30 @@
 """Test binary heap."""
+
+
+import pytest
+
+
+@pytest.fixture
+def empty_heap():
+    """Fixture for empty heap."""
+    from binheap import Binheap
+    heap = Binheap()
+    return heap
+
+
+@pytest.fixture
+def full_heap():
+    """Fixture for full heap."""
+    from binheap import Binheap
+    heap = Binheap([1, 2, 3])
+    return heap
+
+
+def test_empty_heap_is_empty(empty_heap):
+    """Test that empty head has empty list."""
+    assert len(empty_heap.heap) == 0
+
+
+def test_fully_heap_is_full(full_heap):
+    """Test that full heap has data."""
+    assert len(full_heap.heap) == 3
