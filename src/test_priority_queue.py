e9a7a86af52ce79ab61cf56f1eb35a92bb31d07f
Rick Valenzuela <devnull@rickv.com>
2016-12-21 15:10:33 -0800

    added test fixtures, tests for init and insert

---
+++ src/test_priority_queue.py
@@ -0,0 +1,42 @@
+"""Tests for priority queue data structure."""
+
+import pytest
+
+@pytest.fixture
+def empty_pqueue():
+    """Fixture for empty priority queue."""
+    from priority_queue import Priority_Q
+    empty_prio_q = Priority_Q()
+    return prio_q
+
+
+@pytest.fixture
+def populated_pqueue():
+    """Fixture for full heap."""
+    from priority_queue import Priority_Q
+    populated_q = Priority_Q([('Seattle', 1), ('Everett', 3), ('Tacoma', 5)])
+    return populated_q
+
+
+def test_cant_init_w_noniterable():
+    """Test init doesn't work with non-iterable (insert handles tuple part)."""
+    from priority_queue import Priority_Q
+    with pytest.raises(TypeError):
+        Priority_Q(8675309)
+
+
+def test_initted_q_shows_q(populated_pqueue):
+    """Test that populated, initialized queue shows data."""
+    assert populated_pqueue[3] == ('Tacoma', 5)
+
+
+def test_insert_adds_to_q(empty_pqueue):
+    """Test that inserted value is add."""
+    empty_pqueue.insert(('Bellingham', 2))
+    assert empty_pqueue[0] == ('Bellingham', 2)
+
+
+def test_cant_pop_from_empty(empty_pqueue):
+    """Test that pop raises error on empty queue."""
+    with pytest.raises(IndexError):
+        empty_pqueue.pop()
