c769885cd2840b8412cf6b39dab53d445c0a276c
Amos Boldor <amosboldor@gmail.com>
2016-12-22 22:22:56 -0800

    redid binheap

--- src/binheap.py
+++ src/binheap.py
@@ -1,4 +1,4 @@
-"""Create binary heap in which parent nodes are smaller than their children."""
+"""An implementation of a binary heap."""
 
 
 class Binheap(object):
@@ -7,44 +7,51 @@ class Binheap(object):
 
     push() adds a new value into the heap.
 
-    pop() removes the parent value from the top of the heap.
+    pop() removes the parent value from the top of the heap and returns it.
 
     Both push() and pop() maintain heap property.
     """
 
     def __init__(self, iterable=None):
-        """The init function for Binheap."""
+        """Init method for a bin heap class."""
         self.heap = []
-        if iterable and hasattr(iterable, "__iter__"):
-            self.heap.extend(iterable)
-            self.bubble_up()
-        elif iterable:
-            raise TypeError("Can't init with a non iterable.")
+        if iterable is None:
+            iterable = []
+        for val in iterable:
+            self.push(val)
 
-    def push(self, value):
-        """Add a value at the end of the heap into the leftmost empty spot."""
-        self.heap.append(value)
-        if len(self.heap) > 1:
-            self.bubble_up()
+    def push(self, val):
+        """Push will take a value and put it in the bin heap."""
+        self.heap.append(val)
+        self.bubble_up(len(self.heap) - 1)
 
     def pop(self):
-        """Pop the root and return value."""
-        try:
-            popped_value = self.heap[0]
-            self.heap[0], self.heap[-1] = self.heap[-1], self.heap[0]
-            self.heap.pop()
-            if len(self.heap) > 1:
-                self.bubble_up()
-            return popped_value
-        except IndexError:
-            raise IndexError("Can't pop from an empty list.")
+        """Pop will pop the value at the root of the heap."""
+        val = self.heap[0]
+        self.heap[0] = self.heap[len(self.heap) - 1]
+        self.bubble_down(0)
+        self.heap = self.heap[:-1]
+        return val
 
-    def bubble_up(self):
-        """Sort the list."""
-        child = -1
-        parent = int((len(self.heap) - 2) / 2)
-        while self.heap[child] < self.heap[parent]:
-            self.heap[child], self.heap[parent] = self.heap[parent], self.heap[child]
-            child, parent = parent, int((parent - 1) / 2)
-            if self.heap[child] is self.heap[0]:
-                break
+    def bubble_down(self, idx):
+        """Bubble down until meets min heap status."""
+        first_child = (2 * idx) + 1
+        if first_child >= len(self.heap):
+            return
+        second_child = first_child + 1
+        try:
+            child_idx = first_child if self.heap[first_child] < self.heap[second_child] else second_child
+        except IndexError:
+            child_idx = first_child
+        if self.heap[idx] > self.heap[child_idx]:
+            self.heap[idx], self.heap[child_idx] = self.heap[child_idx], self.heap[idx]
+            self.bubble_down(child_idx)
+
+    def bubble_up(self, idx):
+        """Bubble up until meets min heap status."""
+        if idx == 0:
+            return
+        parent_idx = (idx - 1) // 2
+        if self.heap[idx] < self.heap[parent_idx]:
+            self.heap[idx], self.heap[parent_idx] = self.heap[parent_idx], self.heap[idx]
+            self.bubble_up(parent_idx)
