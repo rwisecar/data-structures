9f99cd8dc0295d434f48d9c7ff5f6859b794ba20
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-01-02 13:28:30 -0800

    updating variable names in graph.py

--- src/graph.py
+++ src/graph.py
@@ -28,67 +28,67 @@ class Graph():
         """Instantiate a new graph instance."""
         self.graph = {}
 
-    def add_node(self, n):
+    def add_node(self, node):
         """Add a node to the graph."""
-        if n in self.graph.keys():
+        if node in self.graph.keys():
             raise KeyError("Node already in graph.")
-        self.graph[n] = {"edges": []}
+        self.graph[node] = {"edges": []}
 
-    def add_edge(self, n1, n2):
-        """Add an edge from n1 to n2 in the graph."""
-        if n1 not in self.graph.keys():
-            self.add_node(n1)
-        if n2 not in self.graph.keys():
-            self.add_node(n2)
-        if n2 not in self.graph[n1]['edges']:
-            self.graph[n1]['edges'].append(n2)
+    def add_edge(self, node1, node2):
+        """Add an edge from node1 to node2 in the graph."""
+        if node1 not in self.graph.keys():
+            self.add_node(node1)
+        if node2 not in self.graph.keys():
+            self.add_node(node2)
+        if node2 not in self.graph[node1]['edges']:
+            self.graph[node1]['edges'].append(node2)
 
     def nodes(self):
         """Return a list of all nodes in the graph."""
         node_list = []
-        for n in self.graph.keys():
-            node_list.append(n)
+        for node in self.graph.keys():
+            node_list.append(node)
         return node_list
 
     def edges(self):
         """Return a list of all edges in the graph."""
         edge_list = []
-        for n in self.graph.keys():
-            edge_list.append("{}: {}".format(n, self.graph[n]["edges"]))
+        for node in self.graph.keys():
+            edge_list.append("{}: {}".format(node, self.graph[node]["edges"]))
         return edge_list
 
-    def del_node(self, n):
+    def del_node(self, node):
         """Delete node n from the graph, raises error if does not exist."""
-        if n not in self.graph.keys():
+        if node not in self.graph.keys():
             raise KeyError("You can't delete a node that does not exist.")
-        del self.graph[n]
+        del self.graph[node]
         for key in self.graph.keys():
-            if n in self.graph[key]["edges"]:
-                self.graph[key]["edges"].remove(n)
+            if node in self.graph[key]["edges"]:
+                self.graph[key]["edges"].remove(node)
 
-    def del_edge(self, n1, n2):
+    def del_edge(self, node1, node2):
         """Delete the edge connecting n1 and n2."""
-        if self.graph[n1]['edges'] and self.graph[n2]:
-            self.graph[n1]['edges'].remove(n2)
-        elif n1 not in self.graph.keys() or n2 not in self.graph.keys():
+        if self.graph[node1]['edges'] and self.graph[node2]:
+            self.graph[node1]['edges'].remove(node2)
+        elif node1 not in self.graph.keys() or node2 not in self.graph.keys():
             raise KeyError("That node is not in the graph.")
         else:
             raise ValueError("That edge is not in the graph.")
 
-    def has_node(self, n):
+    def has_node(self, node):
         """Return True if node n is contained in the graph."""
-        return n in self.graph.keys()
+        return node in self.graph.keys()
 
-    def neighbors(self, n):
+    def neighbors(self, node):
         """Return a list of all nodes connected to n by edges."""
-        if n not in self.graph.keys():
+        if node not in self.graph.keys():
             raise KeyError("Not in graph.")
-        return self.graph[n]['edges']
+        return self.graph[node]['edges']
 
-    def adjacent(self, n1, n2):
+    def adjacent(self, node1, node2):
         """Return True if n1 and n2 are connected by an edge."""
-        if n1 not in self.graph.keys():
-            raise KeyError("{} is not in the graph.".format(n1))
-        elif n2 not in self.graph.keys():
-            raise KeyError("{} is not in the graph.".format(n2))
-        return n1 in self.graph[n2]['edges'] or n2 in self.graph[n1]['edges']
+        if node1 not in self.graph.keys():
+            raise KeyError("{} is not in the graph.".format(node1))
+        elif node2 not in self.graph.keys():
+            raise KeyError("{} is not in the graph.".format(node2))
+        return node1 in self.graph[node2]['edges'] or node2 in self.graph[node1]['edges']
