f0d8ea9f3e941bf177cec75bd391b92d134ce8fb
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-01-23 15:22:38 -0800

    adding basic set method

--- src/hash.py
+++ src/hash.py
@@ -13,11 +13,12 @@ class Hash(object):
         self._size = size
         self._hashtable = [[] for n in range(size)]
 
-    def set(key, value):
+    def set(self, key, value):
         """Store the given value using the given key."""
         if type(key) is not str:
             raise TypeError("Hash Tables can only accept strings.")
-        pass
+        key_hash = self._hash(key)
+        self._hashtable[key_hash].append((key_hash, value))
 
     def _hash(self, key):
         """Hash the key provided."""
