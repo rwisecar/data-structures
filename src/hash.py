72ad19d000783ef2aa83e0b31d66900e325b6457
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-01-23 15:10:19 -0800

    setting up framework for hashtable.

---
+++ src/hash.py
@@ -0,0 +1,13 @@
+"""Create a hashtable."""
+
+
+class Hash(object):
+    """Create a hashtable. Its functions are as follows.
+    get(key) - should return the value stored with the given key.
+    set(key, val) - should store the given val using the given key.
+    _hash(key) - should hash the key provided.
+    """
+
+    def __init__(self, size):
+        """Instantiates hashtable instance."""
+        self.hash = [[] for n in range(size)]
