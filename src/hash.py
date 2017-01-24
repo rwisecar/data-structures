c6f6c076fa15b6f7a5bd361966e4ade26796c898
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-01-23 16:43:54 -0800

    added fnv_hash method, still trying to figure out how to pass into methods

--- src/hash.py
+++ src/hash.py
@@ -13,7 +13,19 @@ class Hash(object):
         self._size = size
         self._hashtable = [[] for n in range(size)]
 
-    def set(self, key, value):
+    def _additive_hash(self, key):
+        """Hash the key provided using the additive method."""
+        hash_total = sum([ord(k) for k in key])
+        return hash_total % self._size
+
+    def _fnv_hash(self, key):
+        """Hash the key provided using a modified fnv/xor method."""
+        hash_total = 2166136261
+        for k in key:
+            hash_total = (hash_total * 16777619) ^ ord(k)
+        return hash_total % self._size
+
+    def set(self, key, value, _hash):
         """Store the given value using the given key."""
         if type(key) is not str:
             raise TypeError("Hash tables can only accept strings.")
@@ -25,12 +37,7 @@ class Hash(object):
                     bucket.remove(tup)
         self._hashtable[key_hash].append((key, value))
 
-    def _hash(self, key):
-        """Hash the key provided."""
-        nums = sum([ord(k) for k in key])
-        return nums % self._size
-
-    def get(self, key):
+    def get(self, key, _hash):
         """Return the value stored with the given key."""
         key_hash = self._hash(key)
         bucket = self._hashtable[key_hash]
