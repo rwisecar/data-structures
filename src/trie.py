51f95b9f35ccc90221bbe46cf274a0b6e5b9fb00
Marc Fieser <midfies@gmail.com>
2017-01-25 13:43:39 -0800

    basic structure for trie.py

--- src/trie.py
+++ src/trie.py
@@ -11,8 +11,33 @@ class TrieNode(object):
 
 
 class Trie(object):
-    """An implementation of a trie tree."""
+    """An implementation of a trie tree.
+    insert(self, string): will insert the input string into the trie. If character in the input string is already present, it will be ignored.
+    
+    contains(self, string): will return True if the string is in the trie, False if not.
+    
+    size(self): will return the total number of words contained within the trie. 0 if empty.
+    
+    remove(self, string): will remove the given string from the trie. If the word doesn’t exist, will raise an appropriate exception.
+    """
 
     def __init__(self):
+        """Initialize a trie tree."""
         self.root = TrieNode()
+        self._size = 0
 
+    def insert(self, string):
+        """Insert a string into the trie."""
+        pass
+
+    def contains(self, string):
+        """Return true if string is in trie."""
+        pass
+
+    def size(self):
+        """Return the number of words contained in the trie."""
+        return self._size
+
+    def remove(self, string):
+        """Return the string from the trie if it exists."""
+        pass
