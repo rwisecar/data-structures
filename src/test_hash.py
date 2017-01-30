c2590862db8f017ddfb93bf2241865b21e2a43bd
Marc Fieser <midfies@gmail.com>
2017-01-30 14:46:19 -0800

    added parametrized tables for testing

--- src/test_hash.py
+++ src/test_hash.py
@@ -17,38 +17,48 @@ def test_hash_fnv():
     hash_table = Hash(10, 'fnv')
     return hash_table
 
+ADDITIVE_TEST_TABLE = [
+    ['d', 0],
+    ['e', 1],
+    ['f', 2],
+    ['g', 3],
+    ['h', 4],
+    ['i', 5],
+    ['j', 6],
+    ['k', 7],
+    ['l', 8],
+    ['m', 9],
+]
+
+FNV_TEST_TABLE = [
+    ['f', 0],
+    ['g', 9],
+    ['h', 8],
+    ['i', 7],
+    ['j', 6],
+    ['k', 5],
+    ['l', 4],
+    ['m', 3],
+    ['n', 2],
+    ['o', 1],
+]
+
 
 def test_that_hash_table_has_the_size_given(test_hash):
     """Test that creating a hash gives it a size."""
     assert len(test_hash._hashtable) == 10
 
 
-def test_additive_hash_table_returns_appropriate_hash_value(test_hash):
+@pytest.mark.parametrize("char, result", ADDITIVE_TEST_TABLE)
+def test_additive_hash_table_returns_appropriate_hash_value(char, result, test_hash):
     """Test that the hash function adds value of each char."""
-    assert test_hash._hash("d") == 0
-    assert test_hash._hash("e") == 1
-    assert test_hash._hash("f") == 2
-    assert test_hash._hash("g") == 3
-    assert test_hash._hash("h") == 4
-    assert test_hash._hash("i") == 5
-    assert test_hash._hash("j") == 6
-    assert test_hash._hash("k") == 7
-    assert test_hash._hash("l") == 8
-    assert test_hash._hash("m") == 9
+    assert test_hash._hash(char) == result
 
 
-def test_fnv_hash_table_returns_appropriate_hash_value(test_hash_fnv):
+@pytest.mark.parametrize("char, result", FNV_TEST_TABLE)
+def test_fnv_hash_table_returns_appropriate_hash_value(char, result, test_hash_fnv):
     """Test that the hash function adds value of each char."""
-    assert test_hash_fnv._hash("f") == 0
-    assert test_hash_fnv._hash("g") == 9
-    assert test_hash_fnv._hash("h") == 8
-    assert test_hash_fnv._hash("i") == 7
-    assert test_hash_fnv._hash("j") == 6
-    assert test_hash_fnv._hash("k") == 5
-    assert test_hash_fnv._hash("l") == 4
-    assert test_hash_fnv._hash("m") == 3
-    assert test_hash_fnv._hash("n") == 2
-    assert test_hash_fnv._hash("o") == 1
+    assert test_hash_fnv._hash(char) == result
 
 
 def test_that_setting_a_non_string_throws_type_error(test_hash):
